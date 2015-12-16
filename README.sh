c355dc8c30f8c1e11bfe2c7ede9faf3582848f85
Hirotomo Moriwaki <philopon.dependence@gmail.com>
2015-12-17 01:06:59 +0900

    updated README.md

--- README.sh
+++ README.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
 
-# $ curl -L curl -L https://raw.githubusercontent.com/philopon/.vim/master/README.md | bash
+# $ curl -L curl -L https://raw.githubusercontent.com/philopon/.vim/master/README.sh | bash
 
 cd ~
 git clone git@github.com:philopon/.vim.git
 ln -sf .vim/vimrc .vimrc
 ln -sf .vim/gvimrc .gvimrc
-mkdir .config
+[ ! -d .config ] && mkdir .config
 cd .config
 ln -sf ../.vim nvim
